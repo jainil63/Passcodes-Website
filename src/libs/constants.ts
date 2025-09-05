fc3427cfd3b5bc80c107ff24ed7913cbb796b31b
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-09-05 21:35:15 +0530

    feat: add all import URL in constants.ts

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,6 +1,12 @@
-export default {
-    GITHUB_LATEST_RELEASE_API_URL: "https://api.github.com/repos/JeelDobariya38/Passcodes/releases/latest",
-    GITHUB_RELEASE_URL: "https://github.com/JeelDobariya38/Passcodes/releases",
-    TELEGRAM_COMMUNITY_URL: "https://t.me/passcodescommunity"
-};
+let GITHUB_REPO_SIGNATURE = "PasscodesApp/Passcodes";
 
+export default {
+  GITHUB_REPO_URL: `https://github.com/${GITHUB_REPO_SIGNATURE}`,
+  GITHUB_REPO_FORK_URL: `https://github.com/${GITHUB_REPO_SIGNATURE}/fork`,
+  GITHUB_DOCS_DIR_URL: `https://github.com/${GITHUB_REPO_SIGNATURE}/tree/main/docs`,
+  GITHUB_RELEASE_URL: `https://github.com/${GITHUB_REPO_SIGNATURE}/releases`,
+  GITHUB_LATEST_RELEASE_URL: `https://github.com/${GITHUB_REPO_SIGNATURE}/releases/latest`,
+  GITHUB_LATEST_RELEASE_API_URL: `https://api.github.com/repos/${GITHUB_REPO_SIGNATURE}/releases/latest`,
+  RELEASE_NOTES_URL: `https://github.com/${GITHUB_REPO_SIGNATURE}/blob/main/docs/release-notes.md`,
+  TELEGRAM_COMMUNITY_URL: "https://t.me/passcodescommunity",
+};
